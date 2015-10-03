9209d0d141458c506b9667ddd3596253c5c8949e
Felix Müller <mueller.fe@gmail.com>
2015-10-03 20:09:36 +0200

    more cleanup

--- main.py
+++ main.py
@@ -23,19 +23,9 @@ def gen_adaptive(m,pcs,times,keep_thoughts=False,name="final"):
 		else:
 			cons += (1 - cons)*0.3
 		all_outputs.append(resdata[-1])
-		if keep_thoughts:
-			all_thoughts.append(resdata)
-	noteStateMatrixToMidi(numpy.array(all_outputs),'output/'+name)
-	if keep_thoughts:
-		pickle.dump(all_thoughts, open('output/'+name+'.p','wb'))
 
-def fetch_train_thoughts(m,pcs,batches,name="trainthoughts"):
-	all_thoughts = []
-	for i in range(batches):
-		ipt, opt = multi_training.getPieceBatch(pcs)
-		thoughts = m.update_thought_fun(ipt,opt)
-		all_thoughts.append((ipt,opt,thoughts))
-	pickle.dump(all_thoughts, open('output/'+name+'.p','wb'))
+	writenoteStateMatrixToMidiToFile(numpy.array(all_outputs),'output/'+name)
+
 
 if __name__ == '__main__':
 
