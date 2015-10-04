aa8122d4177d14b9005a631038cddc90576e7d60
Felix Müller <mueller.fe@gmail.com>
2015-10-04 02:14:31 +0200

    Revert "Add compile mode to theano functions"
    This reverts commit df441bca3633fb841ec4d216769ad48e6b6ae14d.

--- model.py
+++ model.py
@@ -5,8 +5,6 @@ from out_to_in_op import OutputFormToInputFormOp
 
 from theano_lstm import Embedding, LSTM, RNN, StackedCells, Layer, create_optimization_updates, masked_loss, MultiDropout
 
-mode = theano.compile.Mode(optimizer="fast_compile", linker="py")
-
 def has_hidden(layer):
     """
     Whether a layer has a trainable
@@ -257,15 +255,13 @@ class Model(object):
             inputs=[self.input_mat, self.output_mat],
             outputs=self.cost,
             updates=updates,
-            allow_input_downcast=True,
-            mode=mode)
+            allow_input_downcast=True)
 
-        print "model-setup-train::Trace-11"
 
         self.update_thought_fun = theano.function(
             inputs=[self.input_mat, self.output_mat],
             outputs= ensure_list(self.time_thoughts) + ensure_list(self.note_thoughts) + [self.cost],
-            allow_input_downcast=True, mode=mode)
+            allow_input_downcast=True)
 
 
 
@@ -359,15 +355,13 @@ class Model(object):
             inputs=[self.steps_to_simulate, self.conservativity, self.predict_seed],
             outputs=self.predicted_output,
             updates=updates,
-            allow_input_downcast=True,
-            mode=mode)
+            allow_input_downcast=True)
 
         self.predict_thought_fun = theano.function(
             inputs=[self.steps_to_simulate, self.conservativity, self.predict_seed],
             outputs=ensure_list(self.predict_thoughts),
             updates=updates,
-            allow_input_downcast=True,
-            mode=mode)
+            allow_input_downcast=True)
 
     def setup_slow_walk(self):
 
@@ -412,8 +406,7 @@ class Model(object):
             inputs=[self.conservativity],
             outputs=slow_walk_results,
             updates=updates,
-            allow_input_downcast=True,
-            mode=mode)
+            allow_input_downcast=True)
 
     def start_slow_walk(self, seed):
         seed = np.array(seed)
