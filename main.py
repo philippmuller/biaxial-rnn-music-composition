5fcd022b350d47a30e511a2959d0a627aaeae05c
Philipp Müller <mailphilippmuller@gmail.com>
2015-10-04 22:10:49 +0200

    fix daemon

--- main.py
+++ main.py
@@ -42,6 +42,13 @@ def web_endpoint(m, pcs):
 	return gen_adaptive(m, pcs, 1, name='live')
 
 class App():
+  def __init__(self):
+    self.stdin_path = '/dev/null'
+    self.stdout_path = '/dev/tty'
+    self.stderr_path = '/dev/tty'
+    self.pidfile_path =  '/tmp/foo.pid'
+    self.pidfile_timeout = 5
+
 	def run(self):
 		pcs = multi_training.loadPieces("more_music")
 		print "--> loaded pieces"
